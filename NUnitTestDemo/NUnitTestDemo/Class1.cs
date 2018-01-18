673b006b35ef5eac0bb1d8115fccd880140e6731
Erkki Nykanen <erkki1502@student.hamk.fi>
2018-01-18 18:13:39 +0200

    NUnit test demo

---
+++ NUnitTestDemo/NUnitTestDemo/Class1.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTestDemo
+{
+    [TestFixture]
+    class Testcase
+    {
+        [TestCase]
+        public void Add()
+        {
+            MyMath math = new MyMath();
+            Assert.AreEqual(5,math.Add(2,3));
+        }
+
+            [TestCase]
+            public void Sub()
+        {
+            MyMath math = new MyMath();
+            Assert.AreEqual(1,math.Sub(3,2));
+        }
+
+    }
+}
