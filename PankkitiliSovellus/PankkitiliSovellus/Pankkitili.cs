7be9fe280de17b4eb5132daa7744645f3cbd9cd3
Erkki <Erkki1502@student.hamk.fi>
2018-01-19 09:44:01 +0200

    Pankkitili test

---
+++ PankkitiliSovellus/PankkitiliSovellus/Pankkitili.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PankkitiliSovellus
+{
+    class Pankkitili
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pankkitilisovellus_v1");
+        }
+    }
+}
---
+++ PankkitiliSovellus/PankkitiliSovellusTests/PankkiTiliTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PankkitiliSovellusTests
+{
+    [TestFixture]
+    public class PankkiTiliTest
+    {
+        /*Pankkitilisovellus sisältää:
+         * 
+         * Luokka, jonka nimi on pankkitili
+         * Pankkitilissä on kolme toimintoa
+         * 
+         * tallettaa rahaa
+         * nostaa rahaa
+         * siirtää rahaa pankkitilitä toiselle
+         * tarkisteta ettei tili mene miinukselle
+        */
+        [Test]
+        public void CreateBackAccount()
+        {
+            Pankkitili tili1 = new Pankkitili();
+            //Assert.That(2, Is.EqualTo(2));
+        }
+
+    }
+}
