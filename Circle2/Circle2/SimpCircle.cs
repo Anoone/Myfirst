fd10d42d647274ee9e6b76360a25857b93f41d53
Erkki Nykanen <erkki1502@student.hamk.fi>
2018-01-26 00:47:13 +0200

    java to c# simplified circle

---
+++ Circle2/Circle2/SimpCircle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle2
+{
+    public class SimpCircle
+    { 
+        private double radius;
+
+        public SimpCircle()
+        {
+            this.radius = 1.0;
+        }
+
+        public SimpCircle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public double getRadius()
+        {
+            return radius;
+        }
+
+        public void setRadius(double newRadius)
+        {
+            radius = newRadius; // radius setter
+        }
+
+        public double getArea()
+        {
+            return radius * radius * Math.PI;
+        }
+
+        public double getCircumference()
+        {
+            return 2 * Math.PI * radius;
+        }
+
+        public String toString()
+        {
+            return "Circle[radius=" + radius + "]";
+        }
+    }
+}
---
+++ Circle2/Circle2/TestSimpCircle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle2
+{
+    public class TestCircle
+    {  
+        public static void Main(String[] args)
+        {
+
+            SimpCircle c1 = new SimpCircle(5.0);
+            Console.WriteLine(c1.toString());
+            Console.WriteLine(" ");
+            Console.WriteLine("The circle has radius of "
+               + c1.getRadius() + " and area of " + c1.getArea() + " and a circumference of " + c1.getCircumference());
+
+
+        }
+    }
+}
