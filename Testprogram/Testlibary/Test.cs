6944564766afc4c936cd50a3204c6a079c493509
a <a>
2018-01-26 09:02:22 +0200

    Added test program (frame)

---
+++ Testprogram/Testlibary/Test.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testlibary
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void test1()
+        {
+            Assert.That(1, Is.EqualTo(1));
+        }
+    }
+}
